3cacd66201f65ca11df32f267e4b4308779faa02
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-06-22 20:12:39 -0400

    Create instagramBot.h

---
+++ instagramBot.h
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+class instagramBot():
+  
+  def __init__(self, email, password):
+    self.browser = webdriver.Chrome()
+    self.email = email
+    self.password = password
+    
+  def signIn():
+  def likePic():
+  def commentOnPic():
+  def 
