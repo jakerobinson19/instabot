5e1e6a9354a76261792d55142593962f85333c84
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-06-30 18:01:05 -0400

    Create commenting_util.py

---
+++ commenting_util.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import random
+import emoji
+
+wordOptions = [['Omg','So,'Absolutely','You are',],['cute','adorable','lovely','so sweet']]
+emojiOptions = [':heart:',':heartpulse:',':two_hearts:',':kissing_heart:',':heart_eyes:','
+
+def createComment():
+  comment = None
+  
+  firstWord = wordOptions[0][random.randint(0,len(wordOptions[0]-1)
+  secondWord = wordOptions[1][random.randint(0,len(wordOptions[1]-1)
+  emoji = emojiOptions[random.randint(0,len(emojiOptions)-1)
+  
+  comment = firstWord + secondWord + emoji
+  return(comment)
+
+def getNumberOfComments():
+
+
+
+def getComments():
+
+
+
