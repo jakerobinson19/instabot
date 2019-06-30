f3a13ff09dec46fd2937c59650d916b5c4cc774f
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-06-30 18:04:07 -0400

    Update commenting_util.py

--- commenting_util.py
+++ commenting_util.py
@@ -8,9 +8,9 @@ emojiOptions = [':heart:',':heartpulse:',':two_hearts:',':kissing_heart:',':hear
 def createComment():
   comment = None
   
-  firstWord = wordOptions[0][random.randint(0,len(wordOptions[0]-1)
-  secondWord = wordOptions[1][random.randint(0,len(wordOptions[1]-1)
-  emoji = emojiOptions[random.randint(0,len(emojiOptions)-1)
+  firstWord = wordOptions[0][random.randint(0,len(wordOptions[0])-1)]
+  secondWord = wordOptions[1][random.randint(0,len(wordOptions[1])-1)]
+  emoji = emojiOptions[random.randint(0,len(emojiOptions)-1)]
   
   comment = firstWord + secondWord + emoji
   return(comment)
