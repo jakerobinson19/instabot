48673d2f2837afe026a9f2a45dbb7a8b26b75b77
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-01 14:26:59 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+likes_per_day = 500
+comments_per_day = 90
+start_time = 
+end_time = 
+follow_back = False
+follower_to_following_ratio_limit = 2
+comments_options = [['words],['more words],[punctuation],[emojis]]
+
+
