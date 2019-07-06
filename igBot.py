eb7b3262f150772a2d697ee58f79629485be3f74
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-06 15:58:39 -0400

    Update igBot.py

--- igBot.py
+++ igBot.py
@@ -1,8 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup as bs
+
+import emoji
 import random
 import time
+import sys
 
 class instagramBot():
   
