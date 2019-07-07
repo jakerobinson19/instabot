411b3c8bef20904357717a84108de19d3243aec2
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-06 20:39:37 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
+import datetime
+
+#apparently 350 likes/hour is the limit
 likes_per_day = 500
 comments_per_day = 90
 start_time = 
 end_time = 
 follow_back = False
-follower_to_following_ratio_limit = 2
+follows_per_day = 10
+follower_to_following_ratio_limit = 3
 comments_options = [['words],['more words],[punctuation],[emojis]]
 
-
+def getConfig():
+  return
