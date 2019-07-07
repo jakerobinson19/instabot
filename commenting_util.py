88967603153ed262dbde2ef408b73133bc0efc37
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-07 17:21:21 -0400

    Update commenting_util.py

--- commenting_util.py
+++ commenting_util.py
@@ -2,17 +2,19 @@ from selenium import webdriver
 import random
 import emoji
 
-wordOptions = [['Omg','So','Absolutely','You are',],['the cutest','adorable','lovely','so sweet']]
-emojiOptions = [':heart:',':heartpulse:',':two_hearts:',':kissing_heart:',':heart_eyes:','
+wordOptions = [['Omg','So','Absolutely','You are',],
+               ['cute','adorable','lovely','so sweet','so so adorable','fabulous'],
+               ['!','!!','!!!']]
+emojiOptions = [':heart:',':heartpulse:',':two_hearts:',':kissing_heart:',':heart_eyes:']
 
 def createComment():
-  comment = None
   
   firstWord = wordOptions[0][random.randint(0,len(wordOptions[0])-1)]
   secondWord = wordOptions[1][random.randint(0,len(wordOptions[1])-1)]
-  emoji = emojiOptions[random.randint(0,len(emojiOptions)-1)]
+  punctuation = self.wordOptions[2][random.randint(0,len(self.wordOptions[2])-1)]
+  #emoji = emojiOptions[random.randint(0,len(emojiOptions)-1)]
   
-  comment = firstWord + ' ' + secondWord + ' ' + emoji
+  comment = firstWord + ' ' + secondWord + punctuation
   return(comment)
 
 def getNumberOfComments():
@@ -20,9 +22,12 @@ def getNumberOfComments():
 def getComments():
 
 def sendComment(self, comm):
-    self.commentInput = self.browser.find_element_by_class_name('Ypffh')
-    self.commentInput.send_keys(comm)
-    delay()
-    #self.commentInputNew = self.browser.find_element_by_xpath("//div[@class='Mfkwx wUsz1']")
-    self.commentInput.send_keys(Keys.ENTER)
+   comment_box = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea.Ypffh")))
+   time.sleep(1)
+   comment_box.click()
+   comment_box = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea.Ypffh")))
+   comment_box.send_keys(comm)
+   
+   comment_box.send_keys(Keys.ENTER)
+   comments = comments + 1
 
