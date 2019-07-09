a93d64c88aa6a1235007d9b0aec40a0a7d580372
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-08 20:52:26 -0400

    Update commenting_util.py

--- commenting_util.py
+++ commenting_util.py
@@ -2,13 +2,12 @@ from selenium import webdriver
 import random
 import emoji
 
-wordOptions = [['Omg','So','Absolutely','You are',],
-               ['cute','adorable','lovely','so sweet','so so adorable','fabulous'],
+wordOptions = [['Omg','So','Absolutely',"You're",],
+               ['so flippin cute','adorable','lovely','so sweet','so so adorable','fabulous'],
                ['!','!!','!!!']]
 emojiOptions = [':heart:',':heartpulse:',':two_hearts:',':kissing_heart:',':heart_eyes:']
 
 def createComment():
-  
   firstWord = wordOptions[0][random.randint(0,len(wordOptions[0])-1)]
   secondWord = wordOptions[1][random.randint(0,len(wordOptions[1])-1)]
   punctuation = self.wordOptions[2][random.randint(0,len(self.wordOptions[2])-1)]
@@ -21,6 +20,7 @@ def getNumberOfComments():
 
 def getComments():
 
+  
 def sendComment(self, comm):
    comment_box = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea.Ypffh")))
    time.sleep(1)
