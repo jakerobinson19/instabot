1b512b437ba1a29eefc9796770afd6e59f8f8846
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-09 22:22:57 -0400

    Update nav.py

--- nav.py
+++ nav.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python
 
+from selenium.webdriver.common.keys import Keys
 
+def scroll(browser):
+    
 
-def right_arrow():
-    self.body = self.browser.find_element_by_tag_name('body')
-    self.body.send_keys(Keys.ARROW_RIGHT)
+def right_arrow(browser):
+    body = browser.find_element_by_tag_name('body')
+    body.send_keys(Keys.ARROW_RIGHT)
 
-def left_arrow():
-    self.body = self.browser.find_element_by_tag_name('body')
-    self.body.send_keys(Keys.ARROW_LEFT)
+def left_arrow(browser):
+    body = browser.find_element_by_tag_name('body')
+    body.send_keys(Keys.ARROW_LEFT)
 
-def go_to_profile(uname = None):
+def go_to_profile(bot, uname = None):
     if uname == None:
-        uname = self.email
-    self.browser.get('https://www.instagram.com/{}'.format(uname))
+        uname = bot.email
+    bot.browser.get('https://www.instagram.com/{}'.format(uname))
   
-def get_username():
-    wait = WebDriverWait(self.browser, 10)
+def get_username(browser):
+    wait = WebDriverWait(browser, 10)
     uname = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'h2.BrX75')))
     return(uname)
 
-def go_to_hashtag(hashtag):
-    self.browser.get('https://www.instagram.com/explore/tags/{}/'.format(hashtag))
+def go_to_hashtag(browser, hashtag):
+    browser.get('https://www.instagram.com/explore/tags/{}/'.format(hashtag))
   
