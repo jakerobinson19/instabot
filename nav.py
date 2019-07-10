d3d17bbda8f9fc592d961520af3b00ae91b44516
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-09 22:19:31 -0400

    Update nav.py

--- nav.py
+++ nav.py
@@ -2,24 +2,24 @@
 
 
 
-def rightArrow():
+def right_arrow():
     self.body = self.browser.find_element_by_tag_name('body')
     self.body.send_keys(Keys.ARROW_RIGHT)
 
-def leftArrow():
+def left_arrow():
     self.body = self.browser.find_element_by_tag_name('body')
     self.body.send_keys(Keys.ARROW_LEFT)
 
-def goToProfile(uname = None):
+def go_to_profile(uname = None):
     if uname == None:
         uname = self.email
     self.browser.get('https://www.instagram.com/{}'.format(uname))
   
-def getUsername():
+def get_username():
     wait = WebDriverWait(self.browser, 10)
     uname = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'h2.BrX75')))
     return(uname)
 
-def goToHashtag(hashtag):
+def go_to_hashtag(hashtag):
     self.browser.get('https://www.instagram.com/explore/tags/{}/'.format(hashtag))
   
