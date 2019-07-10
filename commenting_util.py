7003f13ebb509e67589947aa56f2cc3139da4f03
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-09 22:18:16 -0400

    Update commenting_util.py

--- commenting_util.py
+++ commenting_util.py
@@ -4,14 +4,14 @@ from selenium import webdriver
 import random
 import emoji
 
-wordOptions = [['Omg','So','Absolutely',"You're",],
+word_options = [['Omg','So','Absolutely',"You're",],
                ['so flippin cute','adorable','lovely','so sweet','so so adorable','fabulous'],
                ['!','!!','!!!']]
-emojiOptions = [':heart:',':heartpulse:',':two_hearts:',':kissing_heart:',':heart_eyes:']
+emoji_options = [':heart:',':heartpulse:',':two_hearts:',':kissing_heart:',':heart_eyes:']
 
 def check_if_comments_enabled():
   
-def createComment():
+def create_comment():
   firstWord = wordOptions[0][random.randint(0,len(wordOptions[0])-1)]
   secondWord = wordOptions[1][random.randint(0,len(wordOptions[1])-1)]
   punctuation = self.wordOptions[2][random.randint(0,len(self.wordOptions[2])-1)]
@@ -20,12 +20,12 @@ def createComment():
   comment = firstWord + ' ' + secondWord + punctuation
   return(comment)
 
-def getNumberOfComments():
+def get_number_of_comments():
 
-def getComments():
+def get_comments():
 
   
-def sendComment(self, comm):
+def send_comment(self, comm):
    comment_box = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea.Ypffh")))
    time.sleep(1)
    comment_box.click()
