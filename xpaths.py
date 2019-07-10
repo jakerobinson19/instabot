15db753b84489884ce35984496d9c2ec19155842
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-09 22:00:59 -0400

    Update xpaths.py

--- xpaths.py
+++ xpaths.py
@@ -4,8 +4,8 @@
 xpath = {}
 
 xpath['get_count'] {
-      'followers':'
-      'following':'
+      'followers':'span'
+      'following':'span'
           
 xpath['get_pic'] {
       'all_shown': "//div[@class='v1Nh3 kIKUG  _bz0w']",
@@ -14,7 +14,7 @@ xpath['get_pic'] {
 }
       
 xpath['like_button'] {
-      'like_button':'//span[@class='fr66n'
+      'like_button':"//span[@class='fr66n']"
 }
 
 xpath['status'] {
@@ -24,4 +24,18 @@ xpath['notification_wall'] {
       'not_now':"//button[text()='Not Now']",
       'turn_on':"//div/h2[text()='Turn on Notifications']"
 }
+
+xpath['buttons'] {
+      'first_next':'/html/body/div[3]/div[1]/div/div/a',
+      'next': '/html/body/div[3]/div[1]/div/div/a[2]'
+}
       
+selector['login_elem'] {
+      'username': 'form input',
+      'login': 'form input'
+}
+
+selector['elements'] {
+      'comment_box': "textarea.Ypffh",
+      'profile_username':'h2.BrX75'
+}
