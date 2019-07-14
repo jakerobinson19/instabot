0b5221c4c0283b3934ea7f4fac8eebdbfe063662
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-14 19:24:22 -0400

    Update commenting_util.py

--- instabot/commenting_util.py
+++ instabot/commenting_util.py
@@ -4,31 +4,40 @@ from selenium import webdriver
 import random
 import emoji
 
-word_options = [['Omg','So','Absolutely',"You're",],
-               ['so flippin cute','adorable','lovely','so sweet','so so adorable','fabulous'],
+word_options = ['Omg','So','Absolutely',"Holy fluff. You're", 'Awwww', "By our fluffy paws! You're "],
+               ['so cute','adorable','lovely','so sweet','so so adorable','fabulous'],
                ['!','!!','!!!']]
-emoji_options = [':heart:',':heartpulse:',':two_hearts:',':kissing_heart:',':heart_eyes:']
 
-def check_if_comments_enabled():
+def already_commented_on(self):
+    try:
+      comms = get_comments_on_post()
+      number_of_comms = len(comms)  
+      print("there are {} comments on this post".number_of_comms)
+    except:
+      print("Unable to find any comments on this post")
+
+    already_commented = False
+
+    for c in comms:
+      if c.text == self.email:
+        already_commented = True
+
+    return(already_commented)
   
 def create_comment():
   firstWord = wordOptions[0][random.randint(0,len(wordOptions[0])-1)]
   secondWord = wordOptions[1][random.randint(0,len(wordOptions[1])-1)]
   punctuation = self.wordOptions[2][random.randint(0,len(self.wordOptions[2])-1)]
-  #emoji = emojiOptions[random.randint(0,len(emojiOptions)-1)]
   
   comment = firstWord + ' ' + secondWord + punctuation
   return(comment)
 
-def get_number_of_comments():
-  number_of_comments = read_xpath(['comments']['comments_on_pic'])
-  return(len(comments))
-  #browser.find_elements_by_xpath("//ul[@class='Mr508']")
-  
-def get_comments(browser):
-  comments = read_xpath(['comments']['comments_on_pic'])
-  return(comments)
-  #browser.find_elements_by_xpath("//ul[@class='Mr508']")
+def get_comments_on_post(browser):
+    return(browser.find_elements_by_xpath(read_xpath('pic_comments','comments'))
+
+def get_number_of_comments(browser):
+  num_of_comments = browser.find_elements_by_xpath(read_xpath(['comments']['comments_on_pic']))
+  return(len(num_of_comments))
 
 def get_username_on_comment(comments):
   comment_usernames = []
