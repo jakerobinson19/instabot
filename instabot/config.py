f52783df7f69028150c8667f4987983c469b3490
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-15 22:32:50 -0400

    Update config.py

--- instabot/config.py
+++ instabot/config.py
@@ -1,16 +1,16 @@
 import datetime
 
 #apparently 350 likes/hour is the limit
-likes_per_day = 500
+max_likes_per_day = 500
 comments_per_day = 90
-max_comments_to_post = 10
+max_comments_to_post = 80
 min_comments_to_post = 0
 start_time = 
 end_time = 
 follow_back = False
 follows_per_day = 10
-follower_to_following_ratio_limit = 3
-comments_options = [['words],['more words],[punctuation],[emojis]]
+follower_to_following_ratio_limit = 4
+comments_options = [['words],['more words],[punctuation]]
 
 def getConfig():
   return
