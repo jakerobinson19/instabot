2f4f5330af06da99ce851a2c45966d24a19d1e66
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-16 20:24:14 -0400

    Update config.py

--- instabot/config.py
+++ instabot/config.py
@@ -1,16 +1,38 @@
-import datetime
+mport datetime
 
 #apparently 350 likes/hour is the limit
 max_likes_per_day = 500
-comments_per_day = 90
+comments_per_day = 80
 max_comments_to_post = 80
 min_comments_to_post = 0
 start_time = 0
 end_time = 0
+comment_if_following = False
+like_if_following = True
 follow_back = False
 follows_per_day = 10
-follower_to_following_ratio_limit = 4
+follower_to_following_ratio_limit = 3
 comments_options = [['words],['more words],[punctuation]]
 
+blacklist = ['dachshundoftheday.ig', 'dachshundlife.ig', 'ilovemydaschunddog',
+			 'dachshund_corner', 'cats_addict_01', 'cutedachshundclub', 
+			 'dachshundclothing', 'dachshund_ins.gram', 'dachshund_addict', 
+			 'iris_ivy_violet_mini_sausages', 'dachshundmillie', 'dougalanddapple', 
+			 'patasebicosveterinaria', 'dachshund_catydogy']
+
+yesterdays_hashtags = []
+
+yesterdays_usernames = []
+
+hashtag_list = ['dachshund', 'dachshunds', 'dachshundsonly', 'dachshund_love',
+                'doxiesofig', 'doxiefever', 'dachshundgram', 'dachshundlife',
+                'dachshund_feature','dachshundnation','doxiesofig','sausagedogs',
+                'dachshundsarethebest','dachshundobsessed','doxies', 'dogoftheday',
+                'dachshundoftheday','dachshundmoments','fluffy','dogs',
+                'dachshundlovers','dogphotography','dachshundappreciation','wienerdog',
+                'wienerdogs','wienerdogworld','minisausagedog','weinerdogsofinstagram',
+                'minidoxie','doxiepuppy','miniaturedachshund','dachshund_corner',
+                'dachshundpuppy','dackel','doglover','petstagram','instadog','dogoftheday']
+
 def getConfig():
-  return
+  return(0)
