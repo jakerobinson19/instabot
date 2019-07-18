c20b35763e1df492fd55541e3d44f8d8ba3e8c19
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-17 20:18:46 -0400

    Update nav.py

--- instabot/nav.py
+++ instabot/nav.py
@@ -13,6 +13,9 @@ def left_arrow(browser):
     body = browser.find_element_by_tag_name('body')
     body.send_keys(Keys.ARROW_LEFT)
 
+def go_to_feed(browser)
+    browser.get('https://www.instagram.com/')
+                
 def go_to_profile(bot, uname = None):
     if uname == None:
         uname = bot.email
