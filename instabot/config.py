7cdc05356e85030ec92a2e35bf6e8ad34e3cac16
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-18 21:53:28 -0400

    Update config.py

--- instabot/config.py
+++ instabot/config.py
@@ -1,16 +1,20 @@
 
 
 #apparently 350 likes/hour is the limit
-profiles_per_hash = 8
+profiles_per_hash = 5
 likes_per_profile = 6
 
 max_likes_per_day = 500
 max_comments_per_day = 80
 
-delay_time = 45
+delay_time = 20
+delay_variance = 15
 
 comment_if_following = False
 like_if_following = True
+
+days_to_refresh_blacklist = 5
+
 follow_back = False
 follows_per_day = 10
 follower_to_following_ratio_limit = 3
