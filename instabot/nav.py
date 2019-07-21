0ce3509cf19d575990f275e6188e1f0d32bcffe9
Jake.Robinson <jakerobinson19@gmail.com>
2019-07-21 16:17:15 -0400

    updated code

--- instabot/nav.py
+++ instabot/nav.py
@@ -2,9 +2,6 @@
 
 from selenium.webdriver.common.keys import Keys
 
-def scroll(browser):
-    
-
 def right_arrow(browser):
     body = browser.find_element_by_tag_name('body')
     body.send_keys(Keys.ARROW_RIGHT)
@@ -13,18 +10,13 @@ def left_arrow(browser):
     body = browser.find_element_by_tag_name('body')
     body.send_keys(Keys.ARROW_LEFT)
 
-def go_to_feed(browser)
+def go_to_feed(browser):
     browser.get('https://www.instagram.com/')
                 
 def go_to_profile(bot, uname = None):
     if uname == None:
         uname = bot.email
     bot.browser.get('https://www.instagram.com/{}'.format(uname))
-  
-def get_username(browser):
-    wait = WebDriverWait(browser, 10)
-    uname = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'h2.BrX75')))
-    return(uname)
 
 def go_to_hashtag(browser, hashtag):
     browser.get('https://www.instagram.com/explore/tags/{}/'.format(hashtag))
--- instabot/retrieve.py
+++ instabot/retrieve.py
@@ -1,33 +1,61 @@
+"""selenium modules"""
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
+"""Exceptions"""
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import TimeoutException
+
 from finder_function import read_xpath
 from finder_function import read_selector
 
-def username_from_profile():
+
+
+def username_from_profile(browser):
     wait = WebDriverWait(browser, 10)
     uname = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, read_selector('elements','profile_username'))))
    
     return(uname)
     
-def comments_on_post():
-    time.sleep(2)
+def comments_on_post(browser):
     return(browser.find_elements_by_xpath(read_xpath('comments','comments_on_pic')))
       
-def like_button():
+def like_button(browser):
     wait = WebDriverWait(browser, 10)
     
     like_button = wait.until(EC.visibility_of_element_located((By.XPATH, read_xpath('like_button','like_button'))))
     return(like_button)
     
-def feed_like_buttons():
-    self.hearts = browser.find_elements_by_xpath("//span[@class='fr66n']")
-    return(self.hearts)
+def feed_like_buttons(browser):
+    hearts = browser.find_elements_by_xpath("//span[@class='fr66n']")
+    return(hearts)
     
-def follower_count():
-    time.sleep(1)
-    return(int(browser.find_element_by_partial_link_text("followers").find_element_by_xpath('span').get_attribute('title').replace(',','')))
+def follower_count(browser):
+    follower_element = browser.find_element_by_partial_link_text("followers").find_element_by_xpath('span')
+    follower_count = int(follower_element.get_attribute('title').replace(',',''))
 
-def following_count():
-    time.sleep(1)
-    return(int(browser.find_element_by_partial_link_text("following").find_element_by_xpath('span').text.replace(',','')))
+    return(follower_count)
+
+def following_count(browser):
+    following_element = browser.find_element_by_partial_link_text("following").find_element_by_xpath('span')
+    following_count = int(following_element.get_attribute('title').replace(',',''))
+
+    return(following_count)
     
-def pics_from_profile():
+def pics_from_profile(browser):
     return(browser.find_elements_by_xpath(read_xpath('get_pic','all_shown')))
+
+def get_current_url(browser):
+    """ Get URL of the loaded webpage """
+    try:
+        current_url = browser.execute_script("return window.location.href")
+
+    except WebDriverException:
+        try:
+            current_url = browser.current_url
+
+        except WebDriverException:
+            current_url = None
+
+    return current_url
