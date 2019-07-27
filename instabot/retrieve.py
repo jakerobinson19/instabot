aa86c35e79b54c4af0d6b0defa593cbfea15d818
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-07-27 02:19:54 -0400

    Update retrieve.py

--- instabot/retrieve.py
+++ instabot/retrieve.py
@@ -1,9 +1,6 @@
 """selenium modules"""
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-
 from selenium.webdriver.support import expected_conditions as EC 
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
 """Exceptions"""
