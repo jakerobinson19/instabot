dd2b2b33b6c05f138ccd1a3a14629cf90e27364b
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-08-01 23:09:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,22 @@ from setuptools import setup
 from os import path
 
 summary = "Tool for automated Instagram interactions"
-project_homepage = "https://github.com/timgrossmann/InstaPy"
+project_homepage = "https://github.com/jakerobinson19/instabot"
 here = path.abspath(path.dirname(__file__))
 
-
-
-
 setup(
-    name="instapy",
-    version=metadata["version"],
-    description=summary,
-    long_description=documentation,
-    long_description_content_type="text/markdown",
-    author=u"Tim Großmann",)
+     name='instabot',  
+     version='1.0',
+     scripts=['instabot'] ,
+     author="Jake Robinson",
+     author_email="jakerobinson19@gmail.com",
+     description="Automated bot for instagram using selenium",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url=project_homepage,
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+     ],
+ )
