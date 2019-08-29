29becef8d218686289f23ac722885490e9a578ca
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-08-28 23:22:58 -0400

    Added init_webdriver()

--- instabot/web_nav.py
+++ instabot/web_nav.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
-
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+
+def init_webdriver(file_path):
+  # Start browser in incognito mode to prevent cookies and disabled infobars
+  options = webdriver.ChromeOptions()
+  options.add_argument("incognito")
+  options.add_argument("disable-infobars")
+  driver = webdriver.Chrome(executable_path=file_path, 
+                              chrome_options=options)
+  driver.wait = WebDriverWait(driver, 10)
+  return(driver)
 
 def right_arrow(browser):
     body = browser.find_element_by_tag_name('body')
