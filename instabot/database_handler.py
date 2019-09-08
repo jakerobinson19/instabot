419731cde699ade6d8eb557b39f5253acc7944e6
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-09-07 21:09:39 -0400

    Added add_data, create_tables, select_usernames_for_date, check_if_username_in_table

--- instabot/database_handler.py
+++ instabot/database_handler.py
@@ -1,5 +1,7 @@
 import os
 import sqlite3
+from sqlite3 import ProgrammingError
+import datetime
 
 # create sqlite connection and updates/deletes/gets
 SQL_CREATE_USERNAMES_TABLE = """
@@ -8,26 +10,21 @@ SQL_CREATE_USERNAMES_TABLE = """
         `username` TEXT NOT NULL,
         `followers` INTEGER,
         `following` INTEGER,
-        `f2f ratio` FLOAT,
+        `f2f_ratio` FLOAT,
         `status` TEXT NOT NULL,
-        `date` DATETIME NOT NULL);"""
-
-SQL_CREATE_BLACKLIST_TABLE = """
-    CREATE TABLE IF NOT EXISTS `profiles` (
-        `username` TEXT NOT NULL,
-        'f2f ratio', FLOAT);"""
+        `cal_date` DATETIME NOT NULL);"""
 
 SQL_CREATE_ACCOUNT_PROGRESS_TABLE = """
     CREATE TABLE IF NOT EXISTS `accountProgress` (
-        `date` DATETIME NOT NULL,
+        `cal_date` DATETIME NOT NULL,
         `followers` INTEGER NOT NULL,
         `following` INTEGER NOT NULL,
         `f2f ratio` FLOAT);"""
 
-SQL_CREATE_RECORD_ACTIVITY_TABLE = """
+SQL_CREATE_ACTIVITY_RECORD_TABLE = """
     CREATE TABLE IF NOT EXISTS 'activityRecord' (
-        `date` DATE NOT NULL,
-        `time_elapse` DATETIME NOT NULL,
+        `cal_date` DATE NOT NULL,
+        `time_elapsed` DATETIME NOT NULL,
         `profiles_engaged` INTEGER,
         `comments` INTEGER,
         `likes` INTEGER);"""
@@ -53,9 +50,77 @@ def create_database(address, name):
 
     except Exception as exc:
             print("Wah! Error occurred while getting a DB for '{}':\n\t{}".format(
-                name, str(exc))
+                name, str(exc)))
 
     finally:
         if connection:
             # close the open connection
             connection.close()
+
+def select_usernames_for_date(address, date):
+    names = []
+
+    conn = sqlite3.connect(address)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM usernames WHERE cal_date=:cal_date", {"cal_date":date})
+    
+    rows = cursor.fetchall()
+
+    if conn:
+        conn.close()
+
+    for r in rows:
+        names.append(r[1]) 
+
+    return(names)
+
+def check_if_username_in_table(address, username, date_range):
+    date_range = list(map(str, date_range))
+
+    conn = sqlite3.connect(address)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM usernames WHERE username=:username", {"username":username})
+    records = cursor.fetchall()
+
+    print(records)
+
+    if records:
+        for r in records:
+            if r[6] in date_range:
+                return(True)
+
+    return(False)
+
+
+def create_tables(cursor, tables):
+    if "usernames" in tables:
+        cursor.execute(SQL_CREATE_USERNAMES_TABLE)
+
+    if "recordActivity" in tables:
+        cursor.execute(SQL_CREATE_ACTIVITY_RECORD_TABLE)
+
+    if "accountProgress" in tables:
+        cursor.execute(SQL_CREATE_ACCOUNT_PROGRESS_TABLE)
+
+def add_data(address, table, data):
+
+    conn = sqlite3.connect(address)
+    cursor = conn.cursor()
+
+    try:
+        if table == "usernames":
+            task = "INSERT INTO usernames(username,followers,following,f2f_ratio,status,cal_date) VALUES (?,?,?,?,?,?)"
+
+        elif table == 'accountProgress':
+            task = "INSERT INTO accountProgress(cal_date,followers,following,f2f_ratio) VALUES (?,?,?,?)"
+
+        elif table == 'activityRecord':
+            task = "INSERT INTO activityRecord(cal_date,time_elapsed,profiles_engaged,comments,likes) VALUES (?,?,?,?,?)"
+    except:
+        print('ProgrammingError occurred')
+
+    cursor.execute(task, data)
+
+    conn.commit()
