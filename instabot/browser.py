b923505e2c95c4ff0c83d090af5cd427d0c277c9
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-09-08 22:50:20 -0400

    Update browser.py

--- instabot/browser.py
+++ instabot/browser.py
@@ -1,7 +1,15 @@
-init_driver()
+from selenium import webdriver 
+from selenium.webdriver.support.ui import WebDriverWait
 
-proxy()
+def init_driver(file_path):
+  # Start browser in incognito mode to prevent cookies and disabled infobars
+  options = webdriver.ChromeOptions()
+  options.add_argument("incognito")
+  options.add_argument("disable-infobars")
+  driver = webdriver.Chrome(executable_path=file_path, 
+                              chrome_options=options)
+  driver.wait = WebDriverWait(driver, 10)
+  return(driver)
 
-close_browser()
-  #delete cookies
-  #close
+def close(browser):
+    browser.close()
