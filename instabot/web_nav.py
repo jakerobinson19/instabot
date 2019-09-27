543714b82ec9b526aedac0dad7d1853aa064a443
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-09-27 19:54:35 -0400

    Removed init_driver and close functions

--- instabot/web_nav.py
+++ instabot/web_nav.py
@@ -3,16 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
-def init_webdriver(file_path):
-  # Start browser in incognito mode to prevent cookies and disabled infobars
-  options = webdriver.ChromeOptions()
-  options.add_argument("incognito")
-  options.add_argument("disable-infobars")
-  driver = webdriver.Chrome(executable_path=file_path, 
-                              chrome_options=options)
-  driver.wait = WebDriverWait(driver, 10)
-  return(driver)
-
 def right_arrow(browser):
     body = browser.find_element_by_tag_name('body')
     body.send_keys(Keys.ARROW_RIGHT)
@@ -31,6 +21,3 @@ def go_to_profile(bot, uname = None):
 
 def go_to_hashtag(browser, hashtag):
     browser.get('https://www.instagram.com/explore/tags/{}/'.format(hashtag))
-  
-def close(self):
-    self.browser.close()
