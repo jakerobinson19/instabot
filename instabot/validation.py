a6a40ccb53ea6912a0b78ed3193d9645a5a36073
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-10-02 00:57:10 -0400

    Added validate_username and engagement

--- instabot/validation.py
+++ instabot/validation.py
@@ -4,41 +4,81 @@ import config
 import retrieve
 import comment_util
 
-def validate_username(usname):
-    records = databse_handler.select_username_from_table(usname)
+def validate_username(usname, blacklist, old_usernames):
 
     if usname == config.USERNAME:
+      msg = "----- This is your username -----"
       return(False, msg)
-    elif records:
-      for r in records:
-        if r[4] > config.follower_to_following_ratio_limit:
-          msg = "------ Username is in the blacklist ------"
-          return(False, msg)
-        
-        elif r[6] in date_range:
-          msg = "------ Engaged with this username before ------"
-          return(False, msg)
-        
-        else:
-          return(True, None)
+
+    elif usname in blacklist:
+      msg = "------ Username is in the blacklist ------"
+      return(False, msg)
+
+    elif usname in old_usernames:
+      msg = "------ Engaged with this username before ------"
+      return(False, msg)
+
     else:
       return(True, None)
 
-def validate_engagement(browser, post):
-    follow_stat = retrieve.follow_status(browser)
-    media_type = retrieve.media_type(browser)
+def validate_profile(browser, follow_status):
+    disengage, cant_comment, cant_like = False, False, False
+    msg = "Disengaging"
 
-    if follow_stat:
-      pass
+    if follow_status == 'Follow Back' and not config.INTERACT_IF_FOLLOW_BACK:
+      msg = "Status is Follow Back. Disengage!"
+      disengage = True
+    
+    elif follow_status == 'Following':
+      
+      if not config.COMMENT_IF_FOLLOWING:
+        cant_comment = True
+      
+      if not config.LIKE_IF_FOLLOWING:
+        cant_like = True
 
-    return(exit, comment_allowed, like_allowed)
+    if cant_comment and cant_like:
+      disengage = True
+
+    return(disengage, cant_comment, cant_like, msg)
+
+def validate_engagement(browser, tag):
+  comment_allowed, like_allowed = True, True
+
+  media_type = retrieve.media_type(browser)
+  caption_check = validate_caption(browser)
+  comments = comment_util.get_comments_on_post(browser)
+  
+  if not check_for_hashtag_in_comments(comments, tag):
+    comment_allowed = False
+
+  if caption_check:
+    comment_allowed = False
+
+  if media_type == 'mp4':
+      
+    if not config.COMMENT_ON_VIDEOS:
+      comment_allowed = False
+      
+    if not config.LIKE_VIDEOS:
+      like_allowed = False
+    
+  else:
+      
+    if not config.COMMENT_ON_PICS:
+      comment_allowed = False
+      
+    if not config.LIKE_PICS:
+      like_allowed = False
+
+  return(comment_allowed, like_allowed)
 
 def validate_caption(browser):
     caption = retrieve.pic_caption(browser)
 
     abort = False
     
-    for word in config.ignore_if_contains:
+    for word in config.BAD_WORDS:
       if word in caption:
         print("Bad word in the caption ({})".format(word))
         abort = True
@@ -49,20 +89,29 @@ def already_liked(element):
     not_liked = True
 
     try:
-      heart_button = retrieve.heart_button(element)
+      heart_button = retrieve.heart_outline(element)
     except NoSuchElementException:
       print('Picture already liked, moving on...')
       not_liked = False
 
     return(not_liked)
 
-def already_commented_on(bot):
+def already_commented_on(browser):
     already_commented = False
 
-    comms = comment_util.get_comments_on_post()
+    comms = comment_util.get_comments_on_post(browser)
     
     for c in comms:
       if c.text == config.USERNAME:
         already_commented = True
 
     return(already_commented)
+
+def check_for_hashtag_in_comments(comments, tag):
+    approved = False
+
+    for c in comments:
+      if tag in c.text:
+        return(True)
+
+    return(approved, msg)
