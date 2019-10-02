9d1537dd763b388d27a69f55dae7b8a7789d63be
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-10-02 00:59:47 -0400

    Refactored and cleaned up code

--- instabot/config.py
+++ instabot/config.py
@@ -1,54 +1,57 @@
 #configuration parameters for how the bot will run and what profiles/pics it will engage with
+USERNAME = 'two_wieners'
+PASSWORD = 'WEiner19!'
 
-# Determine if you want the bot to go to each profile to like/comment
-# If set to False, the bot will simply like/comment the post from the explore page for the hashtag
-go_to_profiles = True
+# Set the number of likes and comments you would like to perform for run of the bot 
+MAX_LIKES = 500
+MAX_COMMENTS = 65
 
-# Apparently 350 likes/hour is the limit set by instagram
-# Keep this in mind when setting the parameters for likes_per_hash and delay_time
-profiles_per_hash = 10
-likes_per_profile = 6
+#apparently 350 likes/hour is the limit
+PROFILES_PER_TAG = 5
+LIKES_PER_PROFILE = 3
 
-# Max quotas that the bot will not exceed
-# Bot will stop once it reaches either of these limits within 24 hours
-max_likes_per_day = 500
-max_comments_per_day = 80
+# Set number of hours you would like the bot to run for
+RUN_TIME = 6
 
-delay_time = 35
-delay_variance = 10
+delay_time = 30
+delay_variance = 25
 
-comment_if_following = False
-like_if_following = True
-interact_if_follow_back = False
-comment_on_videos = True
-comment_on_pictures = True
+COMMENT_IF_FOLLOWING = False
+LIKE_IF_FOLLOWING = True
+INTERACT_IF_FOLLOW_BACK = False
+COMMENT_ON_VIDEOS = True
+LIKE_VIDEOS = True
+COMMENT_ON_PICS = True
+LIKE_PICS = True
 
-#if post has higher than these values it will not like or comment
-max_comments_on_post = 15
-max_likes_on_post = 300
+DAYS_TO_REFRESH_USERNAMES = 13
 
-days_to_refresh_username_lists = 5
+FOLLOW_BACK = False
+FOLLOW_PER_DAY = 10
+FOLLOWER_TO_FOLLOWING_RATIO_LIMIT = 3
 
-follow_back = False
-follows_per_day = 10
-follower_to_following_ratio_limit = 3
+BAD_WORDS = ['surgery','dead','rip','sad','death','tears','worst',
+			 'pain','danger','terrible','awful','memory','peace',
+			 'killed','urgent','notice', 'losing','long sleeve', 't-shirt',
+			 'international delivery', 'made in the usa','get it now','life threatening',
+			 'horrific','life-threat']
 
-word_options = [['Omg','So','Absolutely',"Holy fluff. You're", 'Awwww', "By our fluffy paws! You're "],
-               ['so cute','adorable','lovely','so sweet','so so adorable','fabulous'],
-               ['!','!!','!!!']]
-
-bad_words = ['surgery','dead','rip','sad','death','tears','worst',
-             'pain','danger','terrible','awful','memory','peace']
-
-hashtag_list = ['dachshund', 'dachshunds', 'dachshundsonly', 'dachshund_love',
+HASHTAG_LIST = ['dachshund', 'dachshunds', 'dachshundsonly', 'dachshund_love',
                 'doxiesofig', 'doxiefever', 'dachshundgram', 'dachshundlife',
-                'dachshund_feature','dachshundnation','dachshundsofinstagram','sausagedogs',
+                'dachshund_feature','dachshundnation','doxiesofig','sausagedogs',
                 'dachshundsarethebest','dachshundobsessed','doxies', 'dogoftheday',
                 'dachshundoftheday','dachshundmoments','fluffy','dogs',
                 'dachshundlovers','dogphotography','dachshundappreciation','wienerdog',
                 'wienerdogs','wienerdogworld','minisausagedog','weinerdogsofinstagram',
                 'minidoxie','doxiepuppy','miniaturedachshund','dachshund_corner',
-                'dachshundpuppy','dackel','doglover','petstagram','instadog','dogoftheday']
+                'dachshundpuppy','dackel','doglover','petstagram','instadog','dogoftheday',
+                'dogsofinstagram']
 
-def getConfig():
-  return(0)
+USERNAMES_TO_IGNORE = ['lovedachshund.mag', 'doxie.obsessed','dach_usa_love']
+
+# database configuration
+DB_PATH = '/Users/jakerobinson/Desktop/instabot_db.sqlite'
+DB_NAME = 'instabot_db'
+
+# chromedriver path
+CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
