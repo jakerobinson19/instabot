46fe0226a701a596fb1520ad3ea7070f88eeaed4
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-10-02 00:51:07 -0400

    Refactored retrieve.py

--- instabot/retrieve.py
+++ instabot/retrieve.py
@@ -1,7 +1,7 @@
 """selenium modules"""
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC 
 
 """Exceptions"""
 from selenium.common.exceptions import NoSuchElementException
@@ -11,58 +11,67 @@ from selenium.common.exceptions import TimeoutException
 from finder_function import read_xpath
 from finder_function import read_selector
 
+import web_nav
+
 
 def username_from_profile(browser):
-    uname = WebDriverWait(browser, 10).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, read_selector('elements','profile_username')))
-            )
-   
+    try:
+      wait = WebDriverWait(browser, 10)
+      uname = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, read_selector('elements','profile_username'))))
+      uname = uname.text
+      #uname = self.browser.find_element_by_class_name('BrX75').text
+    except TimeoutException:
+      print("Picture doesnt exist")
+      uname = None
+
     return(uname)
     
-def get_usernames_from_comments(browser):
-    comm_unames = WebDriverWait(browser, 10).until(
-                  EC.presence_of_all_elements_located((By.XPATH, read_xpath('comments','comments_on_pic')))
-                )   
-    return(comm_unames)
-  
-def get_comments_on_post(browser):
-    comms = WebDriverWait(browser, 10).until(
-            EC.presence_of_all_elements_located((By.XPATH, read_xpath('comments','comment_section')))
-            )   
-    return(comms)
+def comments_on_post(browser):
+    try:
+      comms = WebDriverWait(self.browser, 10).until(
+      EC.presence_of_all_elements_located((By.XPATH, read_xpath('comments','comment_section')))
+      )
+        
+    except TimeoutException:
+      print("No comments or caption on this post")
+      comms = None
 
+    return(comms)
+      
 def like_button(browser):
-    like_button = WebDriverWait(browser, 10).until(
-                  EC.visibility_of_element_located((By.XPATH, read_xpath('like_button','like_button')))
-                )
+    wait = WebDriverWait(browser, 10)
+    
+    like_button = wait.until(EC.visibility_of_element_located((By.XPATH, read_xpath('like_button','like_button'))))
     return(like_button)
+
+def heart_outline(element):
+    return(element.find_element_by_xpath(read_xpath('like_button','heart_outline')))
     
 def feed_like_buttons(browser):
     hearts = browser.find_elements_by_xpath("//span[@class='fr66n']")
-    
     return(hearts)
     
 def follower_count(browser):
     try:
         follower_element = browser.find_element_by_partial_link_text("followers").find_element_by_xpath('span')
         follower_count = int(follower_element.get_attribute('title').replace(',',''))
-        
+    
     except NoSuchElementException as e:
         print("error occurred: {}".format(e))
         follower_count = 1
-        
+
     return(follower_count)
 
 def following_count(browser):
     following_element = browser.find_element_by_partial_link_text("following").find_element_by_xpath('span')
-    following_count = int(following_element.get_attribute('title').replace(',',''))
+    following_count = int(following_element.text.replace(',',''))
 
     return(following_count)
     
 def pics_from_profile(browser):
     return(browser.find_elements_by_xpath(read_xpath('get_pic','all_shown')))
 
-def get_current_url(browser):
+def current_url(browser):
     """ Get URL of the loaded webpage """
     try:
         current_url = browser.execute_script("return window.location.href")
@@ -80,25 +89,41 @@ def follow_status(browser):
     status = browser.find_element_by_xpath(read_xpath('get_following_status','follow_button_XP')).text
     return(status)
 
-def follower_to_following_ratio(browser, my_name, username):
-    web_nav.go_to_profile(my_name, username)
+def follower_to_following_ratio(browser, username):
+    web_nav.go_to_profile(browser, username)
 
     try:
-      followers = follower_count()
-      following = following_count()
-        
+      followers = follower_count(browser)
+      following = following_count(browser)
+
     except NoSuchElementException:
       browser.execute_script("location.reload()")
+      
       try:
-        followers = follower_count()
-        following = following_count()
-        
+        followers = follower_count(browser)
+        following = following_count(browser)
+
       except NoSuchElementException:
         print("this user has no followers")
         following = 1
 
     f_ratio = round(followers/following,3)
-    return(f_ratio)
+
+    return(followers, following, f_ratio)
+
+def pic_caption(browser):
+    try:
+      cap = WebDriverWait(browser, 10).until(
+      EC.presence_of_all_elements_located((By.XPATH, read_xpath('comments','comment_section')))
+      )   
+
+    except TimeoutException:
+      print("No caption on this post")
+      cap = None
+    
+    cap = cap[0].text
+    
+    return(cap.lower())
 
 def datetime_of_post(browser):
     t = browser.find_element_by_xpath(read_xpath('post','timestamp')).get_attribute('datetime')
@@ -111,4 +136,5 @@ def media_type(browser):
     except NoSuchElementException:
         pass
 
-    return(type))
+    return(type)
+
