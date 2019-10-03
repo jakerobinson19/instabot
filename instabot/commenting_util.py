b7ef7a53a81c1b2eb7c84754ffabc8465173f78d
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-10-03 01:05:11 -0400

    Update commenting_util.py

--- instabot/commenting_util.py
+++ instabot/commenting_util.py
@@ -1,42 +1,43 @@
 #!/usr/bin/env python
 
-#selenium modules
-from selenium.webdriver.common.keys import Keys
+#!/usr/bin/env python
 from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 
-#instabot modules
-from config import wordOptions
-from config import commenting_params
-from read_functions import read_xpath
+from finder_function import read_xpath
+from finder_function import read_selector
 
-#plus a little randomness
+import time
 import random
 
-def already_commented_on(users_email):
-    already_commented = False
-
-    comms = get_comments_on_post()
-    
-    for c in comms:
-      if c.text == users_email:
-        already_commented = True
-
-    return(already_commented)
+word_options = [['Wow, ','So','Absolutely',"Holy fluff. You're", 'Awwww', "By our fluffy paws! You're ", 'Absocutely'],
+               ['so cute','adorable','lovely','so sweet','so so adorable','fabulous'],
+               ['! ','!! ','!!! ']]
+emoji_options = ['❤️', '❤️🥰❤️', '❤️❤️❤️', '💕💕💕', '🔥', '🔥🔥' ,'😍', '😍😍', '😍🥰', '🥰💕']
   
 def create_comment():
-  firstWord = wordOptions[0][random.randint(0,len(wordOptions[0])-1)]
-  secondWord = wordOptions[1][random.randint(0,len(wordOptions[1])-1)]
-  punctuation = self.wordOptions[2][random.randint(0,len(self.wordOptions[2])-1)]
+  firstWord = word_options[0][random.randint(0,len(word_options[0])-1)]
+  secondWord = word_options[1][random.randint(0,len(word_options[1])-1)]
+  punctuation = word_options[2][random.randint(0,len(word_options[2])-1)]
   emoji = emoji_options[random.randint(0,len(emoji_options)-1)]
   
-  comment = firstWord + ' ' + secondWord + punctuation + ' ' + emoji + ' '
+  comment = firstWord + ' ' + secondWord + punctuation + emoji + ' '
+
   return(comment)
 
+def get_usernames_from_comments(browser):
+    comm_unames = WebDriverWait(browser, 10).until(
+    EC.presence_of_all_elements_located((By.XPATH, read_xpath('comments','comments_on_pic')))
+  )   
+    return(comm_unames)
+  
 def get_comments_on_post(browser):
     try:
-      comms = WebDriverWait(self.browser, 10).until(
+      comms = WebDriverWait(browser, 10).until(
       EC.presence_of_all_elements_located((By.XPATH, read_xpath('comments','comment_section')))
       )   
     except TimeoutException:
@@ -45,59 +46,51 @@ def get_comments_on_post(browser):
 
     return(comms)
 
-def get_number_of_comments():
-  num_of_comments = len(get_comments_on_post()) - 1
+def get_number_of_comments(browser):
+  num_of_comments = len(get_comments_on_post(browser)) - 1
   return(num_of_comments)
 
 def get_username_on_comment(comments):
-    try:
-        comm_unames = WebDriverWait(browser, 10).until(
-        EC.presence_of_all_elements_located((By.XPATH, read_xpath('comments','comments_on_pic')))
-        )
-    except TimeoutException:
-        print("Unable to grab usernames from comments - error occurred")
-        comm_unames = None
-    
-    return(comm_unames)
+  comment_usernames = []
   
-def send_comment(browser, comm):
-   comment_box = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea.Ypffh")))
-   time.sleep(1)
-   comment_box.click()
-   comment_box = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea.Ypffh")))
-   browser.execute_script(
+  for comm in comments:
+    uname = comm.read_xpath(['comments']['username_of_comment'])
+    comment_usernames.append(uname)
+    
+  return(comment_usernames)
+  
+def send_comment(browser, comm=None):
+  if not comm:
+    comm = create_comment()
+
+  comment_box = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea.Ypffh")))
+  time.sleep(1)
+  comment_box.click()
+  comment_box = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea.Ypffh")))
+  browser.execute_script(
                 "arguments[0].value = arguments[1];",
                 comment_box, comm)
 
+  time.sleep(1)
+  #self.commentInputNew = self.browser.find_element_by_xpath("//div[@class='Mfkwx wUsz1']")
   comment_box.send_keys('\b')
   comment_box = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, read_selector('elements','comment_box'))))
+  time.sleep(2)
   comment_box.send_keys(Keys.ENTER)
-  comments = comments + 1
 
 def is_commenting_disabled(browser):
     try:
-        is_disabled = browser.execute_script("return window._sharedData.entry_data.PostPage[0].graphql.shortcode_media.comments_disabled")
+      is_disabled = browser.execute_script("return window._sharedData.entry_data.PostPage[0].graphql.shortcode_media.comments_disabled")
     except:
-        try:
-            browser.execute_script("location.reload()")
-            is_disabled = browser.execute_script("return window._sharedData.entry_data.PostPage[0].graphql.shortcode_media.comments_disabled")  
-        except Exception as e:
+      try:
+        browser.execute_script("location.reload()")
+        is_disabled = browser.execute_script("return window._sharedData.entry_data.PostPage[0].graphql.shortcode_media.comments_disabled")  
+      except Exception as e:
             msg = ("Failed to check comments' status for verification!\n\t{}"
                    .format(str(e).encode("utf-8")))
             return False, msg
     
-    if is_disabled:
+    if is_disabled is True:
       return True, 'Comment are disabled for this post'
 
     return False, "Comments Enabled"
-
-def validate_commenting(post, media_type):
-    if commenting_params['ENABLE_COMMENTS']:
-        if commenting_params['COMMENT_ON_PICS'] and media_type == 'pic':
-            comment_allowed = True
-        elif commenting_params['COMMENT_ON_PICS'] and media_type == 'video':
-            comment_allowed = True
-        else:
-            comment_allowed = False
-            
-    return(comment_allowed)
