de12a8f78e3f738cfcbcc8e896a112be343f0331
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-10-02 22:30:38 -0400

    Added select_pic_on_explore_page functionality

--- instabot/web_nav.py
+++ instabot/web_nav.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+
 from selenium.webdriver.common.keys import Keys
+import retrieve
+import config
+
+web = {}
+
+web['url'] = {
+	'login':'https://www.instagram.com/accounts/login/',
+	'feed': 'https://www.instagram.com/',
+	'hashtag':'https://www.instagram.com/explore/tags/'
+}
 
 def right_arrow(browser):
     body = browser.find_element_by_tag_name('body')
@@ -11,13 +20,21 @@ def left_arrow(browser):
     body = browser.find_element_by_tag_name('body')
     body.send_keys(Keys.ARROW_LEFT)
 
+def go_to_url(browser, page):
+	browser.get(page)
+
 def go_to_feed(browser):
-    browser.get('https://www.instagram.com/')
+    browser.get(web['url']['feed'])
                 
-def go_to_profile(bot, uname = None):
+def go_to_profile(browser, uname = None):
     if uname == None:
-        uname = bot.email
-    bot.browser.get('https://www.instagram.com/{}'.format(uname))
+        uname = config.USERNAME
+    browser.get('https://www.instagram.com/{}'.format(uname))
 
 def go_to_hashtag(browser, hashtag):
     browser.get('https://www.instagram.com/explore/tags/{}/'.format(hashtag))
+
+def select_pic_on_explore_page(browser, ptype):
+    pic = retrieve.pic_from_explore(browser, ptype)
+    pic.click()
+  
