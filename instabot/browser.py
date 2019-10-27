67cbf431cfeb8a7227fcb479f14ebd994a791178
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-10-27 17:46:45 -0400

    Headless browser option added

--- instabot/browser.py
+++ instabot/browser.py
@@ -1,11 +1,14 @@
-from selenium import webdriver 
+from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
-def init_driver(file_path):
+def init_driver(file_path, headless):
   # Start browser in incognito mode to prevent cookies and disabled infobars
   options = webdriver.ChromeOptions()
   options.add_argument("incognito")
-  options.add_argument("disable-infobars")
+  
+  if headless:
+  	options.add_argument("headless")
+  	
   driver = webdriver.Chrome(executable_path=file_path, 
                               chrome_options=options)
   driver.wait = WebDriverWait(driver, 10)
